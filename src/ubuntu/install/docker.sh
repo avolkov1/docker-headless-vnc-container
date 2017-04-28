d0d3e4f32194ea225c05b11d354eff4b639231eb
Douglas Holt <dholt@nvidia.com>
2017-04-28 16:49:32 +0000

    add -y flag

--- src/ubuntu/install/docker.sh
+++ src/ubuntu/install/docker.sh
@@ -9,5 +9,5 @@ apt-get clean -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update 
-apt-get install docker-ce
+apt-get install -y docker-ce
 apt-get clean -y
